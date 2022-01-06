cd6cf1e7f4697cfa44c2673f8beed18863938010
Barış <alibrs15@hotmail.com>
2022-01-06 21:42:12 +0300

    Update q1.py

--- day1/q1.py
+++ day1/q1.py
@@ -2,4 +2,4 @@ def check(input):
   with open(input) as input:
     numbers = [int(line.rstrip()) for line in input]
 
-  return sum([1 for i in range(len(numbers)-1) if numbers[i] < numbers[i+1]])
+  return sum([1 for i in range(len(numbers)-1) if numbers[i]<numbers[i+1]])
